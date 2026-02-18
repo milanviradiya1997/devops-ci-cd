4ea8af0da4674a769a654763d5f9e191826f9ddd
Milan Viradiya <milanviradiya97@gmail.com>
2026-02-18 16:34:13 +0530

    This is a static Flask dashboard used for DevOps CI/CD practice.

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
+import socket
+import datetime
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template(
+        "index.html",
+        hostname=socket.gethostname(),
+        time=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
